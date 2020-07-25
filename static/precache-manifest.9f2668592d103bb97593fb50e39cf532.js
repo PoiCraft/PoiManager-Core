ffeb6447055fb5dc833640d9b9ec09cfeba14f33
Cian John <topjohncian@gmail.com>
2020-07-25 23:48:44 +0800

    update frontend

--- static/precache-manifest.9f2668592d103bb97593fb50e39cf532.js
+++ static/precache-manifest.9f2668592d103bb97593fb50e39cf532.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f950df200fc0acf169af9c60f171f5af",
+    "revision": "54b9a48f4634925f67c1c27b80baf5de",
     "url": "/index.html"
   },
   {
-    "revision": "0f7438f9334ace4f2813",
-    "url": "/static/js/2.6a75e7e7.chunk.js"
+    "revision": "49ad7a6cb3a792553ae4",
+    "url": "/static/js/2.07c530a4.chunk.js"
   },
   {
     "revision": "d79df6f5193a359dc75a31038a5eb669",
-    "url": "/static/js/2.6a75e7e7.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.07c530a4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a341d83edf80d87616ff",
-    "url": "/static/js/main.0538bf54.chunk.js"
+    "revision": "4c650cc7fb4fe2546253",
+    "url": "/static/js/main.a756adf0.chunk.js"
   },
   {
     "revision": "7ffa86fb1529be07f65f",
