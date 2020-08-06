ed7f181100195f24e6342cdbf9d73b873a438632
gggxbbb <2331490629@qq.com>
2020-08-06 11:13:43 +0800

    improve api_cmd.py

--- api/api_cmd.py
+++ api/api_cmd.py
@@ -1,6 +1,6 @@
 import time
 
-from flask import Flask
+from flask import Flask, request
 from api.api import BasicApi
 from auth.Token import TokenManager
 from core.bds import BdsCore
@@ -28,20 +28,39 @@ class Api_Cmd(BasicApi):
             self.bds.sent_to_all('cmd_in', cmd)
             self.bds.cmd_in(cmd)
 
-            time.sleep(0.3)
-
-            if timeout:
-                time.sleep(timeout)
-
             _logs = []
-            index = -1
-            while True:
-                _log = BdsLogger.get_log_all('bds')[index]
-                if _log.time >= cmd_in_time:
-                    _logs.append(_log)
-                    index -= 1
-                else:
-                    break
+
+            line = request.args.get('line', None)
+            if line is None:
+                time.sleep(0.3)
+                if timeout:
+                    time.sleep(timeout)
+
+                index = -1
+                while True:
+                    _log = BdsLogger.get_log_all('bds')[index]
+                    if _log.time >= cmd_in_time:
+                        _logs.append(_log)
+                        index -= 1
+                    else:
+                        break
+            else:
+                line = int(line)
+                while True:
+                    __logs = []
+                    index = -1
+                    _n = True
+                    while _n:
+                        _log = BdsLogger.get_log_all('bds')[index]
+                        if _log.time >= cmd_in_time:
+                            print(index)
+                            __logs.append(_log)
+                            index -= 1
+                        else:
+                            _n = False
+                    if len(__logs) == line:
+                        _logs = __logs
+                        break
 
             for i in range(len(_logs)):
                 logs.append(
